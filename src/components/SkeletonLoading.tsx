e9957e6ae439fc4a20089d4dc0e980a89f9bb5b9
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-06-21 00:34:09 +0530

    refactor: Update SkeletonLoading component styles and class names

--- src/components/SkeletonLoading.tsx
+++ src/components/SkeletonLoading.tsx
@@ -1,10 +1,10 @@
 const SkeletonCard = () => {
   return (
-    <div className="border-2 rounded-lg">
-      <div className="h-48 w-full rounded-t-lg bg-gray-200"></div>
+    <div className="border-2 rounded-lg animate-pulse">
+      <div className="h-48 w-full rounded-t-lg bg-gray-100"></div>
       <div className="p-4 flex-1 flex flex-col gap-4 h-60">
-        <h2 className="h-8 bg-gray-200"></h2>
-        <p className="h-20 bg-gray-200"></p>
+        <h2 className="h-8 bg-gray-100"></h2>
+        <p className="h-20 bg-gray-100"></p>
       </div>
     </div>
   );
