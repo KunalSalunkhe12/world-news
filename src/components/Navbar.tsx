70160a13b58c526cc1aefd177dd95a608a2d8621
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-06-20 18:25:15 +0530

    chore: Add formatDate utility function

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="sticky top-0 right-0 px-6 md:px-12 py-5 bg-black text-white text-3xl">
-      <p className="font-bold">World News 📰</p>
+      <Link to="/" className="font-bold">
+        World News 📰
+      </Link>
     </nav>
   );
 };
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+export function formatDate(inputDate: string) {
+  const date = new Date(inputDate);
+
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+}
